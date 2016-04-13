6ad3072b7d99a9ff22cfa63e38d59ec76b7499b6
gireesh_sk <gireesh_sk@spanservices.com>
2016-04-13 14:55:16 +0530

    RestComm Maday IOS Version

---
+++ ios/MayDay/Sample/AgentWebRTC/AgentWebRTC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AgentWebRTC
+//
+//  Created by Securepms on 14/02/16.
+//  Copyright © 2016 span. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ios/MayDay/Sample/Customer/WEBRTC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WEBRTC
+//
+//  Created by Securepms on 09/02/16.
+//  Copyright © 2016 span. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
